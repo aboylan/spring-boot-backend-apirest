5fa02a3f88ea209b1c4d0a9db02eaddc3a8525bb
Alex Boylan <aboylan@users.noreply.github.com>
2023-10-02 21:30:28 -0600

    Devolviendo inserts originales en el archivo import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -11,14 +11,3 @@ INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Dr. James', 'G
 INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Magma', 'Lee', 'bruce.lee@gmail.com', '2023-09-29');
 INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Tornado', 'Roe', 'john.roe@gmail.com', '2023-09-29');
 INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Jade', 'Doe', 'jane.doe@gmail.com', '2023-09-29');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Mr. John', 'Doe', 'john.doe@gmail.com', '2023-09-29');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Linus', 'Torvalds', 'linus.torvalds@gmail.com', '2023-09-29');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Rasmus', 'Lerdorf', 'rasmus.lerdorf@gmail.com', '2023-09-29');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Erich', 'Gamma', 'erich.gamma@gmail.com', '2023-09-29');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Richard', 'Helm', 'richard.helm@gmail.com', '2023-09-29');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Ralph', 'Johnson', 'ralph.johnson@gmail.com', '2023-09-29');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('John', 'Vlissides', 'john.vlissides@gmail.com', '2023-09-29');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Dr. James', 'Gosling', 'james.gosling@gmail.com', '2023-09-29');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Magma', 'Lee', 'bruce.lee@gmail.com', '2023-09-29');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Tornado', 'Roe', 'john.roe@gmail.com', '2023-09-29');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Jade', 'Doe', 'jane.doe@gmail.com', '2023-09-29');
